0a30f5b2a5430b8596c10c271038f8765c55fc2a
Alexis Besson <alexis.besson@arneogroup.com>
2025-08-23 18:07:06 +0200

    Fix: Check if NVM is loaded, then install/use version 24.6.0 then execute project scripts

--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
+echo "Change Node version to latest Node LTS version (Gallium - 16.x.x)"
+
+NODEVERSION=$(node -v)
+
+if [[ $NODEVERSION != "v24."* ]]
+then
+    [ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
+    nvm install 24.6.0
+    nvm use 24.6.0
+fi
+
 corepack enable pnpm && corepack install -g pnpm@10.15.0
 npm install pm2 -g
