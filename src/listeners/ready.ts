47876eb9944951aff56df9a2c839769dd092e139
Alexis Besson <alexis.besson@arneogroup.com>
2025-08-23 13:36:32 +0200

    Feature: Create "Ready" listener to synchronize DB and register cron

---
+++ src/listeners/ready.ts
@@ -0,0 +1,35 @@
+import type { Client } from 'discord.js';
+import { Listener } from '@sapphire/framework';
+import cron from 'node-cron';
+
+// Config
+import { getLogger } from '@/config/logger.js';
+
+// Models
+import ObjectiveModel from '@/models/objective.model.js';
+
+// Tasks
+import { cronTask } from '@/tasks/cron.js';
+
+export class ReadyListener extends Listener {
+  public constructor(context: Listener.LoaderContext, options: Listener.Options) {
+    super(context, {
+      ...options,
+      once: true,
+      event: 'ready',
+    })
+  }
+
+  public override run(client: Client<true>) {
+    const logger = getLogger();
+    const { displayName } = client.user!;
+
+    logger.info(`Logged in as ${displayName}`);
+
+    ObjectiveModel.sync();
+
+    logger.info(`All models were synchronized successfully`);
+
+    cron.schedule('* * * * *', () => cronTask(logger, client));
+  }
+}
