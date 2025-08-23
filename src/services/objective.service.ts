9adb4047dfb9db0aa8fb39a6ecd162c541b68837
Alexis Besson <alexis.besson@arneogroup.com>
2025-08-23 13:20:12 +0200

    Feature: Implement Models and Services

---
+++ src/services/objective.service.ts
@@ -0,0 +1,40 @@
+import type { FindOptions, InferAttributes } from 'sequelize';
+
+// Models
+import ObjectiveModel, { type Objective } from '@/models/objective.model.js';
+
+export const findAllObjective = async (options?: FindOptions<InferAttributes<Objective>>) => ObjectiveModel.findAll(options);
+
+export const findObjectiveByGuildId = async (guildId: string): Promise<Objective[]> => ObjectiveModel.findAll({
+  order: [
+    ['time', 'ASC'],
+  ],
+  where: {
+    guildId,
+  }
+});
+
+export const findObjectiveById = async (id: string): Promise<Objective[]> => ObjectiveModel.findAll({
+  where: {
+    id,
+  }
+});
+
+export const createObjective = async (
+  data: Pick<Objective, 'guildId' | 'channelId' | 'userId' | 'type' | 'rarity' | 'map' | 'time' | 'maintenanceAdded'>
+): Promise<Objective> => ObjectiveModel.create({
+  guildId: data.guildId,
+  channelId: data.channelId,
+  userId: data.userId,
+  type: data.type,
+  rarity: data.rarity,
+  map: data.map,
+  time: data.time,
+  maintenanceAdded: data.maintenanceAdded,
+});
+
+export const deleteObjective = async (id: number): Promise<number> => ObjectiveModel.destroy({
+  where: {
+    id,
+  }
+});
