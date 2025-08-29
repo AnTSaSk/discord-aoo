b6416c0b11fd7879bf5c76e5ced2a90677a89b0c
Alexis Besson <alexis.besson@arneogroup.com>
2025-08-29 19:06:34 +0200

    Feature: Correctly implement Logtail and extract Logtail endpoint to env file

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -5,24 +5,30 @@ let logger: Logger;
 
 export const getLogger = (): Logger => {
   if (!logger) {
+    let target = 'pino-pretty';
+    let options: Record<string, (boolean | string) | Record<string, boolean | string>> = {
+      colorize: true,
+      ignore: "pid,hostname",
+      translateTime: "dd-mm-yyyy hh:MM:ss TT",
+      levelFirst: true,
+      minimumLevel: "trace",
+    };
+
+    if (!process.env.APP_DEV_MODE) {
+      target = '@logtail/pino';
+      options = {
+        sourceToken: String(process.env.APP_LOGTAIL_TOKEN),
+        options: {
+          endpoint: String(process.env.APP_LOGTAIL_ENDPOINT),
+        }
+      };
+    }
+
     logger = pino({
       level: 'info',
       transport: {
-        target: process.env.APP_DEV_MODE ? 'pino-pretty' : '@logtail/pino',
-        options: process.env.APP_DEV_MODE
-          ? {
-            colorize: true,
-            ignore: "pid,hostname",
-            translateTime: "dd-mm-yyyy hh:MM:ss TT",
-            levelFirst: true,
-            minimumLevel: "trace",
-          }
-          : {
-            sourceToken: process.env.APP_LOGTAIL_TOKEN,
-            options: {
-              endpoint: 'https://s1462358.eu-nbg-2.betterstackdata.com',
-            }
-          },
+        target: target,
+        options: options,
       },
     });
   }
