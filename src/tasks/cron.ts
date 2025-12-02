ba5382cdd80838bb31350c48ded7d99bb8e6b404
Alexis Besson <alexis.besson@arneogroup.com>
2025-12-02 13:41:24 +0100

    Fix: Add await on "for" loop to avoid weird side-effect during CRON tasks

--- src/tasks/cron.ts
+++ src/tasks/cron.ts
@@ -2,7 +2,7 @@ import utc from 'dayjs/plugin/utc.js';
 import dayjs from 'dayjs';
 import { MessageFlags, type Client } from 'discord.js';
 import type { Logger } from 'pino';
-import { Op } from 'sequelize';
+import { Op } from '@sequelize/core';
 
 // Services
 import { deleteObjective, findAllObjective, findObjectiveByGuildId } from '@/services/objective.service.js';
@@ -23,37 +23,33 @@ export const cronTask = async (logger: Logger, client: Client<true>) => {
     ],
     where: {
       time: {
-        [Op.lt]: now.toDate(),
+        [Op.lte]: now.toDate(),
       }
     },
   });
-
-  // NO OBJECTIVE
-  if (objectives.length <= 0) {
-    logger.info('CRON - No objective');
-    logger.info('-- END CRON --');
-
-    return;
-  }
-
   let guildData: Record<string, string>[] = [];
 
-  logger.info('CRON - Delete old objective(s)');
+  // NO PAST OBJECTIVE
+  if (objectives.length <= 0) {
+    logger.info('CRON - No past objective');
+  } else {
+    logger.info('CRON - Delete old objective(s)');
 
-  // Delete objectives
-  for (const objective of objectives) {
-    try {
-      await deleteObjective(objective.id);
+    // Delete objectives
+    for await (const objective of objectives) {
+      try {
+        await deleteObjective(objective.id);
 
-      if (!guildData.some((item) => item.guildId === objective.guildId)) {
-        guildData.push({
-          guildId: objective.guildId,
-          channelId: objective.channelId,
-        });
+        if (!guildData.some((item) => item.guildId === objective.guildId)) {
+          guildData.push({
+            guildId: objective.guildId,
+            channelId: objective.channelId,
+          });
+        }
+      } catch (e) {
+        logger.error(`Error during the delete of the objective: ${objective};
+          Error: ${e}`);
       }
-    } catch (e) {
-      logger.error(`Error during the delete of the objective: ${objective};
-        Error: ${e}`);
     }
   }
 
@@ -61,9 +57,7 @@ export const cronTask = async (logger: Logger, client: Client<true>) => {
   if (guildData) {
     logger.info('CRON - Loop on each Guild data with an updated list');
 
-    for (const key in guildData) {
-      const data = guildData[key];
-
+    for await (const data of guildData) {
       if (data) {
         const channel = await client.channels.cache.get(data.channelId);
         const objectives = await findObjectiveByGuildId(data.guildId);
